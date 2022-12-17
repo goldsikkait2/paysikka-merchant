fc22588cc4f0cb9a40c557dc2b010540fcec3de8
goldsikka <72186070+goldsikkait2@users.noreply.github.com>
2022-12-17 09:54:08 +0530

    update

--- src/app/core/firstpagepromotion/firstpagepromotion.component.ts
+++ src/app/core/firstpagepromotion/firstpagepromotion.component.ts
@@ -11,11 +11,14 @@ import { AuthService } from 'src/app/_services/auth.service';
 export class FirstpagepromotionComponent implements OnInit {
   lengthofPomotions: any;
   lengthofPomotionslength: any;
+  listpromotions: any;
+  arryObj: any;
 
   constructor(private authserv: AuthService, private api:ApiService) { }
 
   ngOnInit(): void {
     this.getListOfPromotions();
+    this.getRecentPurchasedList();
   }
   getListOfPromotions(){
     const currentUser = this.authserv.getUser();
@@ -24,9 +27,30 @@ export class FirstpagepromotionComponent implements OnInit {
     this.api.get(`promotions/list`,{headers}).subscribe((r: any) => {
       this.lengthofPomotions = r;
       this.lengthofPomotionslength = this.lengthofPomotions.length
-    console.log("lenghtoftotalpromotions" ,this.lengthofPomotionslength);
+    console.log("list of promotions" ,this.lengthofPomotions);
     
       
     });
   }
+  getRecentPurchasedList(){
+    const currentUser = this.authserv.getUser();
+    console.log("rencent pruchased inside");
+    
+    let headers= new HttpHeaders().set('Authorization',currentUser.token)
+    this.api.get(`promotions/purchaselist`,{headers}).subscribe((r: any) => {
+      console.log("rencent pruchased inside two ", r);
+      this.listpromotions = r;
+
+      this.listpromotions[0].status===1
+      if(this.listpromotions[0].status===1){
+        console.log("status active");
+        
+      }
+     
+    // console.log("array objects" ,this.listpromotions[0].status);
+   
+    
+      
+    });
+}
 }
